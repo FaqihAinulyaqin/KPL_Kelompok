a7634151e70dda3c5513636eb9124df1c52bfca4
new name <new email>
2024-03-31 23:37:58 +0700

    menambahkan class <NAMA_CLASS>

--- tpmodul7_kelompok_4/Program.cs
+++ tpmodul7_kelompok_4/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+
+public class DataMahasiswa1302220063
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public Nama nama { get; set; }
+    public long NIM { get; set; }
+    public string fakultas { get; set; }
+
+    public DataMahasiswa1302220063() { }
+
+    public static DataMahasiswa1302220063 FromJsonString(string jsonString)
+    {
+        return JsonSerializer.Deserialize<DataMahasiswa1302220063>(jsonString);
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Nama {nama.depan} {nama.belakang} dengan nim {NIM} dari fakultas {fakultas}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonString = "{\"nama\": {\"depan\": \"Irham\", \"belakang\": \"Baehaqi\"}, \"NIM\": 1302220063, \"fakultas\": \"Informatika\"}";
+
+        DataMahasiswa1302220063 mahasiswa = DataMahasiswa1302220063.FromJsonString(jsonString);
+
+        mahasiswa.PrintData();
+    }
+}
