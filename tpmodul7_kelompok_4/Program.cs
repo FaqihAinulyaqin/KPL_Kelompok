df6ef67f4b13929deaf25632b343ffa690ca66b3
FaqihAinulyaqin <mfaqihay@gmail.com>
2024-03-31 23:10:20 +0700

    menambahkan class <DataMahasiswa_1302220086_Praktikan>

--- tpmodul7_kelompok_4/Program.cs
+++ tpmodul7_kelompok_4/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.Json;
+
+public class DataMahasiswa_1302220086_PRAKTIKAN
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+    public Nama nama { get; set; }
+    public int nim { get; set; }
+    public string fakultas { get; set; }
+
+    public DataMahasiswa_1302220086_PRAKTIKAN() { }
+    public static DataMahasiswa_1302220086_PRAKTIKAN FromJsonString(string jsonString)
+    {
+        return JsonSerializer.Deserialize<DataMahasiswa_1302220086_PRAKTIKAN>(jsonString);
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Nama {nama.depan} {nama.belakang} dengan nim {nim} dari fakultas{fakultas}");
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string jsonString = "{\"nama\": {\"depan\": \"Muhammad Faqih\", \"belakang\": \"Ainulyaqin\"}, \"nim\": 1302220086, \"fakultas\": \"Informatika\"}";
+
+        DataMahasiswa_1302220086_PRAKTIKAN mahasiswa = DataMahasiswa_1302220086_PRAKTIKAN.FromJsonString(jsonString);
+
+        mahasiswa.PrintData();
+    }
+}
