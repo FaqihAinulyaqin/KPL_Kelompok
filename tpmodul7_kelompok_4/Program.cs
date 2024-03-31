a9a789ec8a14a563f898940accad9feba4680384
naufalammarr <135613598+naufalammarr@users.noreply.github.com>
2024-04-01 00:28:06 +0700

    menambahkan class KuliahMahasiswa1302220052

--- tpmodul7_kelompok_4/Program.cs
+++ tpmodul7_kelompok_4/Program.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.VisualBasic;
-using System;
+﻿using System;
 using System.Text.Json;
-using static DataMahasiswa1302220052;
 
 public class DataMahasiswa1302220052
 {
@@ -15,28 +13,58 @@ public class DataMahasiswa1302220052
     public long nim { get; set; }
     public string fakultas { get; set; }
 
-   public void ReadJSON(string jsonString)
+    public void ReadJSON(string jsonString)
     {
         DataMahasiswa1302220052 data = JsonSerializer.Deserialize<DataMahasiswa1302220052>(jsonString);
         nama = data.nama;
         nim = data.nim;
         fakultas = data.fakultas;
     }
+
     public void PrintData()
     {
         Console.WriteLine($"Nama {nama.depan} {nama.belakang} dengan nim {nim} dari fakultas {fakultas}");
     }
 }
+
+public class KuliahMahasiswa1302220052
+{
+    public class MataKuliah
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public MataKuliah[] courses { get; set; }
+
+    public void ReadJSON(string jsonString)
+    {
+        KuliahMahasiswa1302220052 data = JsonSerializer.Deserialize<KuliahMahasiswa1302220052>(jsonString);
+        courses = data.courses;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Daftar mata kuliah yang diambil:");
+        for (int i = 0; i < courses.Length; i++)
+        {
+            Console.WriteLine($"MK {i + 1} {courses[i].code} - {courses[i].name}");
+        }
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        string jsonString = "{\"nama\": {\"depan\": \"Naufal Ammar\", \"belakang\": \"Zaidan\"}, \"nim\": 1302220052, \"fakultas\": \"Informatika\"}";
-
-        
+        string mahasiswaJson = "{\"nama\": {\"depan\": \"Naufal Ammar\", \"belakang\": \"Zaidan\"}, \"nim\": 1302220052, \"fakultas\": \"Informatika\"}";
         DataMahasiswa1302220052 mahasiswa = new DataMahasiswa1302220052();
-        mahasiswa.ReadJSON(jsonString);
-
+        mahasiswa.ReadJSON(mahasiswaJson);
         mahasiswa.PrintData();
+
+        string kuliahJson = "{\"courses\": [{\"code\": \"CRI2C4\", \"name\": \"Konstruksi Perangkat Lunak\"}, {\"code\": \"CRI2E4\", \"name\": \"Basis Data\"}, {\"code\": \"UWJXF2\", \"name\": \"Kewirausahaan\"}, {\"code\": \"CII3B4\", \"name\": \"Pemrograman Berorientasi Objek\"}, {\"code\": \"CRJ2H3\", \"name\": \"Pengalaman Pengguna\"}, {\"code\": \"CRI2F2\", \"name\": \"Proyek Tingkat II\"}, {\"code\": \"CRJ2G3\", \"name\": \"Dasar Jaringan Komputer\"}]}";
+        KuliahMahasiswa1302220052 kuliah = new KuliahMahasiswa1302220052();
+        kuliah.ReadJSON(kuliahJson);
+        kuliah.PrintData();
     }
 }
--- tpmodul7_kelompok_4/obj/Debug/net8.0/tpmodul7_kelompok_4.AssemblyInfo.cs
+++ tpmodul7_kelompok_4/obj/Debug/net8.0/tpmodul7_kelompok_4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul7_kelompok_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884611dbb5a0af1619e3fee4ad96100fa33614fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b10f0c8429b2c0eecc826e26698b602df46c3cab")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul7_kelompok_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul7_kelompok_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
